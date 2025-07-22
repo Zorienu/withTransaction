95186afdc65cafc42662264c928ef2bb0108d0a6
Jasyd <jasydavid45@gmail.com>
2025-07-21 19:12:43 -0500

    creds

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,9 +5,9 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'canals',
-  password: 'local',
-  database: 'localtest',
+  username: 'YOUR_USERNAME',
+  password: 'YOUR_PASSWORD',
+  database: 'YOUR_DATABASE',
   synchronize: true, // solo en desarrollo
   logging: false,
   entities: [User],
